0ddf60671b9a743a94e50022cdca9ad43cd79f9e
syrcrk <syrcrk@gmail.com>
2021-01-24 11:27:02 +0800

    移动

---
+++ Java/Spring/Projects/MybatisAnotation/src/main/java/com/spring/mybatis1/mybatisano/MybatisanoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mybatis1.mybatisano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisanoApplication.class, args);
+    }
+
+}
---
+++ Java/Spring/Projects/MybatisAnotation/src/test/java/com/spring/mybatis1/mybatisano/MybatisanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mybatis1.mybatisano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisanoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
